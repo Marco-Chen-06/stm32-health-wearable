add82aee818f5c6b7bf4b2acf0693259418554be
Marco-Chen-06 <marcochen.work@gmail.com>
2026-06-30 17:31:27 -0400

    update thresholds and gyro threshold bug

--- Core/Inc/algorithms.h
+++ Core/Inc/algorithms.h
@@ -11,10 +11,11 @@
 #include "math.h"
 
 #define BMI270_COUNTS_PER_G 4096 // for +-8g, this is 32768/8, or, the acc_mag at rest (1g)
+#define BMI270_COUNTS_PER_DEG 16.4 // for +- 2000 dps range, this is the LSB per degree
 #define WEIGHTLESS_THRESHOLD ((BMI270_COUNTS_PER_G * 8) / 10) // 0.8g = 3276
-#define IMPACT_THRESHOLD ((BMI270_COUNTS_PER_G * 14) / 10)
+#define IMPACT_THRESHOLD ((BMI270_COUNTS_PER_G * 22) / 10) // impact threshold: research paper says 1.4g, ill try 2.2g
 #define STABILITY_THRESHOLD_ACC (BMI270_COUNTS_PER_G / 10)
-#define STABILITY_THRESHOLD_GYR 10 // degrees per second
+#define STABILITY_THRESHOLD_GYR (BMI270_COUNTS_PER_DEG * 20) // degrees per second: research paper says 10 deg/sec, ill try 20
 // number of samples to collect for stability checking
 #define NUM_STABILITY_SAMPLES 200
 // number of stability samples to check
